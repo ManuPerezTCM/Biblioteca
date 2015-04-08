eefc93e2419d9f6042289165f0dae4f81762a63b
xavier <xavier.castilla@eupmt.tecnocampus.cat>
2015-04-08 16:37:37 +0200

    començo amb el controlador

---
+++ Biblioteca/src/CapaAplicacio/controladorFerPrestec.java
@@ -0,0 +1,13 @@
+package CapaAplicacio;
+
+public class controladorFerPrestec {
+	
+	private Soci soci;
+	private Date data_prestec;
+	private Date data_max_retorn;
+	private Exemplar exemplar;
+	
+	public controladorFerPrestec(){
+		
+	}
+}

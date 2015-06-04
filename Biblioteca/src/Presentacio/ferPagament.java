14efdfc2498107aa3f6b7e3a7ec90e32977bf35f
manupereztcm <manuel.perez.belmont@INF2-10.tcm.edu>
2015-06-04 14:01:55 +0200

    Manu: Ultim commit a classe

--- Biblioteca/src/Presentacio/ferPagament.java
+++ Biblioteca/src/Presentacio/ferPagament.java
@@ -13,6 +13,7 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
+import javax.swing.JList;
 
 public class ferPagament extends JFrame {
 
@@ -27,34 +28,69 @@ public class ferPagament extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JLabel lblSoci = new JLabel("Soci: ");
+		JLabel lblSoci = new JLabel("Introdueix un Soci: ");
 		lblSoci.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		
 		textFieldSoci = new JTextField();
 		textFieldSoci.setColumns(10);
 		
 		JButton btnValidar = new JButton("Validar");
+		
+		JList list = new JList();
+		
+		JLabel lblSeleccionaElPrstec = new JLabel("Selecciona el pr\u00E9stec a pagar:");
+		
+		JLabel lblImportACobrar = new JLabel("Import a cobrar:");
+		
+		JLabel lblImport = new JLabel("");
+		
+		JButton btnCobrar = new JButton("Cobrar");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(89, Short.MAX_VALUE)
-					.addComponent(lblSoci)
-					.addGap(18)
-					.addComponent(textFieldSoci, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnValidar)
-					.addGap(105))
+					.addContainerGap(38, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(lblSoci)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textFieldSoci, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
+							.addGap(18))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(list, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(lblSeleccionaElPrstec, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(lblImportACobrar)
+							.addGap(8)))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblImport, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnValidar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnCobrar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGap(51))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(24)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblSoci)
+						.addComponent(btnValidar)
 						.addComponent(textFieldSoci, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnValidar))
-					.addContainerGap(214, Short.MAX_VALUE))
+						.addComponent(lblSoci))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+							.addComponent(lblSeleccionaElPrstec)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(list, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGap(111)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblImportACobrar)
+								.addComponent(lblImport, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addComponent(btnCobrar)))
+					.addGap(39))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
